5512e3401fd99d3bd060fe31d7594eb780e26aed
Andrew Brookins <a.m.brookins@gmail.com>
2019-12-20 13:57:49 -0800

    Tweaks

--- amazon.py
+++ amazon.py
@@ -1,7 +1,10 @@
 import csv
-from decimal import *
 import locale
-locale.setlocale( locale.LC_ALL, '' )
+from decimal import Decimal
+
+
+locale.setlocale(locale.LC_ALL, '')
+
 
 class Order:
     def __init__(self, order_id):
@@ -11,65 +14,97 @@ class Order:
     def add_item(self, item):
         self.items.append(item)
 
+    @property
     def total_price(self):
         total = 0
         for i in self.items:
-            total += i.price
+            total += i.price * i.quantity
         return float(total)
 
+    @property
     def total_items(self):
         return len(self.items)
 
+    @property
+    def order_date(self):
+        return self.items[0].purchase_date
+
+    @property
+    def categories(self):
+        return {i.category for i in self.items}
+
     def __repr__(self):
-        return "Order {0} ; {1} items (${2})".format(self.order_id, self.total_items(), self.total_price())
+        return "Order {}, {} items (${}) [{}]\nItems: {}".format(
+            self.order_date, self.total_items, self.total_price,
+            ', '.join(self.categories),
+            ', '.join([i.name for i in self.items]))
+
 
 class Item:
-    def __init__(self, item_price, item_name, purchase_date, category):
+    def __init__(self, item_price, item_name, purchase_date, category,
+                 quantity):
         self.price = Decimal(item_price[1:])
         self.name = str(item_name)
         self.purchase_date = purchase_date
         self.category = category
+        self.quantity = int(quantity)
 
     def __repr__(self):
-        return "Item: {0} (${1})".format(self.name, self.price)
+        return "Item: {} (${}) x{}".format(self.name, self.price,
+                                              self.quantity)
 
 
 def load_orders_from_csv():
-    csv_file = open('amazon.csv', 'rb')
-    reader = csv.reader(csv_file)
+    csv_file = open('amazon.csv', 'r')
+    reader = csv.DictReader(csv_file)
 
     orders = {}
 
-    first_row_skipped = False
     for row in reader:
-        if not first_row_skipped:
-            first_row_skipped = True
-            continue
-        
-        order_id = row[1]
-        i = Item(item_price=row[8], item_name=row[2], purchase_date=row[0], category=row[3])
-        
+        order_id = row['Order ID']
+        i = Item(item_price=row['Purchase Price Per Unit'],
+                 item_name=row['Title'],
+                 purchase_date=row['Order Date'],
+                 category=row['Category'],
+                 quantity=row['Quantity'])
+
         if not order_id in orders:
             orders[order_id] = Order(order_id)
         orders[order_id].add_item(i)
     return orders
 
 
-orders = load_orders_from_csv()
+def main():
+    orders = load_orders_from_csv()
+    total_sum = 0
+    breakdown = {}
 
-total_sum = 0
-breakdown = {}
+    for order in orders:
+        total_sum += orders[order].total_price
 
-for order in orders:
-    total_sum += orders[order].total_price()
-    for item in orders[order].items:
-        if item.category not in breakdown:
-            breakdown[item.category] = 0
-        breakdown[item.category] += item.price
+        for item in orders[order].items:
+            if item.category not in breakdown:
+                breakdown[item.category] = 0
+            breakdown[item.category] += item.price
 
-for category, price in breakdown.iteritems():
-    if category == '':
-        category = 'None'
-    print "{0}: {1}".format(category, locale.currency( price, grouping=True ))
+    print("\nCategory breakdown:")
+    print("-------------------")
+    for category, price in breakdown.items():
+        if category == '':
+            category = 'None'
+        print("{}: {}".format(category, locale.currency(price,
+                                                          grouping=True)))
 
-print "\nTotal: {0}".format(locale.currency( total_sum, grouping=True ))
+    print("\nTotal spent:")
+    print("------------")
+    print("{}".format(locale.currency(total_sum, grouping=True)))
+
+    print("\nOrders:")
+    print("-------")
+    for order in orders.values():
+        print(order)
+        print()
+
+
+if __name__ == '__main__':
+    main()
